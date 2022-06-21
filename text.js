05c6ee9ae776f15bd1d9a0f4e25bfe99e184bcfb
Eraxus1 <107717374+Eraxus1@users.noreply.github.com>
2022-06-21 16:20:29 +0300

    Add files via upload

---
+++ text.js
@@ -0,0 +1,3 @@
+function devam() {
+    document.getElementById("demo").innerHTML = "Ben Oğuz Can 19 yaşında üniversite öğrencisiyim. <br><br> Web tasarımı konusunda bir çok konuya hakimiyetim var. <br><br> HTML CSS  ve Frontend'e  yetecek kadar JavaScript diline hakimim. <br> <br> Aynı zamanda PHP programlama dilinde mevcut bilgilerim vardır. <br> <br>"
+}
